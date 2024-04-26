dbeb9b92fa16f07e474fd35c3bbc25d0304e5355
Irfan Khan <zackmarts2738@gmail.com>
2024-04-26 18:19:27 +0500

    refactored

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI("AIzaSyDKM-enVJYATTV-Rmdmgh7sOrJ45ZVnUmA");
 
 async function run() {
-  // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-
   const prompt = "as a young investor where should i invest my savings";
-
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
